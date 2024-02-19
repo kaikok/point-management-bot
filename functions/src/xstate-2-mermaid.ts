e3f470fc1d052f4936fc556f0c9336bab34be3f5
Kai Kok, Chew <kaikok@gmail.com>
2024-02-19 17:27:08 +0800

    Split out mermaid generation from traversing XState state machine

--- functions/src/xstate-2-mermaid.ts
+++ functions/src/xstate-2-mermaid.ts
@@ -21,7 +21,7 @@ const eventLabelTransformers = [
   },
 ];
 
-export const transformEventLabel = (label: string) => {
+export const transformXStateEventLabel = (label: string) => {
   let transformed = label;
   eventLabelTransformers.forEach((transformer) => {
     if (transformer.active) {
@@ -36,45 +36,111 @@ export const transformEventLabel = (label: string) => {
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getInitialState = (stateMachineNode: any) => {
+export const getXStateInitialState = (stateMachineNode: any) => {
   return stateMachineNode.states[stateMachineNode.config.initial];
 };
 
 // Mermaid generation routines
 const generateStateDeclaration = (
-  stringTokens: string[],
   identation: number,
-  id: any,
-  key: any) => {
+  id: string,
+  key: string) => {
+  const stringTokens: Array<string> = [];
   stringTokens.push(
     "  ".repeat(identation) +
     `${id} : ${key}\n`);
+  return stringTokens;
 };
 
-const generateCompositeStatePreamble = (
+const generateTransitionToFinal = (
   identation: number,
-  id: any, stateMachineNode: any) => {
+  id: string) => {
   const stringTokens: Array<string> = [];
+  stringTokens.push(
+    "  ".repeat(identation) +
+    `${id} --> [*]\n`);
+  return stringTokens;
+};
+
+const generateCompositeState = (
+  states: { [x: string]: any; },
+  identation: number,
+  id: any,
+  initialStateId: string,
+  forEachSubState: (
+    states: any,
+    identation: number,
+    callbackFn:
+      (stateMachineNode: any, identation: number, first: boolean,
+        visitNodeFn:
+          (stateMachineNode: any, identation: number) => Array<string>)
+      => Array<string>,
+      visitNodeFn:
+        (stateMachineNode: any, identation: number)
+        => Array<string>) => Array<string>,
+  visitNodeFn:
+    (stateMachineNode: any, identation: number) => Array<string>):
+    Array<string> => {
+  let stringTokens: Array<string> = [];
   stringTokens.push("  ".repeat(identation) +
     `state ${id} {\n`);
+
   stringTokens.push("  ".repeat(identation + 1) +
     "[*] --> " +
-    `${getInitialState(stateMachineNode).id}\n`);
+    `${initialStateId}\n`);
+
+  stringTokens = stringTokens.concat(
+    forEachSubState(states, identation, generateInternalState, visitNodeFn));
+
+  stringTokens.push("  ".repeat(identation) + "}\n");
   return stringTokens;
 };
 
 const generateInternalState = (
   stateMachineNode: any,
   identation: number,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   first: boolean) => {
-  return recurseXStateNode(stateMachineNode,
+  return visitXStateNode(stateMachineNode,
     identation);
 };
 
-const generateConcurrentState = (
+const generateConcurrentStates = (
+  states: any,
+  identation: number,
+  id: any,
+  forEachSubState: (
+    states: any,
+    identation: number,
+    callbackFn:
+      (stateMachineNode: any, identation: number, first: boolean,
+        visitNodeFn:
+          (stateMachineNode: any, identation: number) => Array<string>)
+      => Array<string>,
+      visitNodeFn:
+        (stateMachineNode: any, identation: number)
+        => Array<string>) => Array<string>,
+  visitNodeFn:
+    (stateMachineNode: any, identation: number) => Array<string>):
+    Array<string> => {
+  let stringTokens: Array<string> = [];
+  stringTokens.push("  ".repeat(identation) +
+      `state ${id} {\n`);
+
+  stringTokens = stringTokens.concat(
+    forEachSubState(
+      states, identation, generateEachConcurrentState, visitNodeFn));
+
+  stringTokens.push("  ".repeat(identation) + "}\n");
+  return stringTokens;
+};
+
+const generateEachConcurrentState = (
   stateMachineNode: any,
   identation: number,
-  first: boolean) => {
+  first: boolean,
+  visitNodeFn:
+    (stateMachineNode: any, identation: number) => Array<string>) => {
   let stringTokens: Array<string> = [];
   if (!first) {
     stringTokens.push(
@@ -82,17 +148,21 @@ const generateConcurrentState = (
       "--\n");
   }
   stringTokens = stringTokens.concat(
-    recurseXStateNode(stateMachineNode,
+    visitNodeFn(stateMachineNode,
       identation));
   return stringTokens;
 };
 
-const enumerateXStateSubStates = (
+const forEachXStateSubStates = (
   states: { [x: string]: any; },
   identation: number,
-  enumerator:
-    (stateMachineNode: any, identation: number, first: boolean)
-    => Array<string>) => {
+  callbackFn:
+    (stateMachineNode: any, identation: number, first: boolean,
+      visitNodeFn:
+        (stateMachineNode: any, identation: number) => Array<string>
+      ) => Array<string>,
+  visitNodeFn:
+    (stateMachineNode: any, identation: number) => Array<string>) => {
   let stringTokens: Array<string> = [];
   let first = true;
   if (states) {
@@ -101,7 +171,8 @@ const enumerateXStateSubStates = (
       if (Object.prototype.hasOwnProperty.call(
         stateName, idx)) {
         stringTokens = stringTokens.concat(
-          enumerator(states[stateName[idx]], identation + 1, first));
+          callbackFn(
+            states[stateName[idx]], identation + 1, first, visitNodeFn));
         if (first) {
           first = false;
         }
@@ -111,9 +182,61 @@ const enumerateXStateSubStates = (
   return stringTokens;
 };
 
-const enumerateXStateTransitions = (
-  transitions: any, parent: any, identation: number, id: any) => {
+const generateTransition = (
+  currentStateId: string,
+  targetStateId: string,
+  identation: number,
+  eventName: string, guard: string) => {
   const stringTokens: Array<string> = [];
+  stringTokens.push("  ".repeat(identation) +
+    `${currentStateId} --> ${targetStateId} :` +
+    ` ${eventName}` +
+    `${guard === "" ? "" : "/" + guard}\n`);
+  return stringTokens;
+};
+
+const forEachXStateSiblingState = (
+  siblingStates: any,
+  currentState: any,
+  identation: number,
+  eventName: string,
+  guard: string,
+  callbackFn: (
+    currentStateId: string,
+    targetStateId: string,
+    identation: number,
+    eventName: string,
+    guard: string) => Array<string>) => {
+  let stringTokens: Array<string> = [];
+  if (siblingStates) {
+    const stateName = Object.keys(siblingStates);
+    for (const stateIdx in stateName) {
+      if (Object.prototype.hasOwnProperty.call(
+        stateName, stateIdx)) {
+        if (currentState.id !== siblingStates[stateName[stateIdx]].id) {
+          stringTokens = stringTokens.concat(callbackFn(
+            siblingStates[stateName[stateIdx]].id,
+            currentState.id,
+            identation,
+            transformXStateEventLabel(eventName),
+            guard ? guard : ""));
+        }
+      }
+    }
+  }
+  return stringTokens;
+};
+
+const forEachXStateTransitions = (
+  transitions: any, parent: any,
+  identation: number, id: any,
+  callbackFn: (
+    currentStateId: string,
+    targetStateId: string,
+    identation: number,
+    eventName: string,
+    guard: string) => Array<string>) => {
+  let stringTokens: Array<string> = [];
   for (const [eventName, transition] of transitions) {
     const targets = transition[0].target;
     const guard = transition[0].guard;
@@ -122,30 +245,21 @@ const enumerateXStateTransitions = (
       if (Object.prototype.hasOwnProperty.call(targets, idx)) {
         const srcParentId = parent.id;
         const targetParentId = targets[idx].parent.id;
+        // Check for within same state machine
         if (srcParentId === targetParentId) {
-          stringTokens.push(
-            "  ".repeat(identation) +
-            `${id} --> ${targets[idx].id} :` +
-            ` ${transformEventLabel(eventName)}` +
-            `${guard ? "/" + guard : ""}\n`);
+          stringTokens = stringTokens.concat(callbackFn(
+            id,
+            targets[idx].id,
+            identation,
+            transformXStateEventLabel(eventName), guard ? guard : ""));
         } else {
-          const siblingStates = targets[idx].parent.states;
-          if (siblingStates) {
-            const stateName = Object.keys(siblingStates);
-            for (const stateIdx in stateName) {
-              if (Object.prototype.hasOwnProperty.call(
-                stateName, stateIdx)) {
-                if (targets[idx].id !== siblingStates[stateName[stateIdx]].id) {
-                  stringTokens.push(
-                    "  ".repeat(identation) +
-                    `${siblingStates[stateName[stateIdx]].id} -->` +
-                    ` ${targets[idx].id} :` +
-                    ` ${transformEventLabel(eventName)}` +
-                    `${guard ? "/" + guard : ""}\n`);
-                }
-              }
-            }
-          }
+          stringTokens = stringTokens.concat(forEachXStateSiblingState(
+            targets[idx].parent.states,
+            targets[idx],
+            identation,
+            transformXStateEventLabel(eventName),
+            guard ? guard : "",
+            callbackFn));
         }
       }
     }
@@ -153,81 +267,39 @@ const enumerateXStateTransitions = (
   return stringTokens;
 };
 
-export const recurseXStateNode = (
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  stateMachineNode: any, identation: number) => {
+export const visitXStateNode = (
+  stateMachineNode: any, identation: number): Array<string> => {
   let stringTokens: Array<string> = [];
-  const {id, type, transitions, parent, states} = stateMachineNode;
+  const {id: currentId, type, transitions, parent, states} = stateMachineNode;
 
-  generateStateDeclaration(
-    stringTokens,
-    identation,
-    id,
-    stateMachineNode.key);
+  stringTokens = stringTokens.concat(
+    generateStateDeclaration(
+      identation, currentId, stateMachineNode.key));
 
   if (type === "compound") {
     stringTokens = stringTokens.concat(
-      generateCompositeStatePreamble(identation, id, stateMachineNode));
-
-    stringTokens = stringTokens.concat(
-      enumerateXStateSubStates(states, identation, generateInternalState));
-    stringTokens.push("  ".repeat(identation) + "}\n");
+      generateCompositeState(
+        states, identation, currentId,
+        getXStateInitialState(stateMachineNode).id,
+        forEachXStateSubStates, visitXStateNode));
   }
 
   if (type === "parallel") {
-    stringTokens.push("  ".repeat(identation) +
-        `state ${id} {\n`);
     stringTokens = stringTokens.concat(
-      enumerateXStateSubStates(states, identation, generateConcurrentState));
-    stringTokens.push("  ".repeat(identation) + "}\n");
+      generateConcurrentStates(
+        states, identation, currentId,
+        forEachXStateSubStates, visitXStateNode));
   }
 
-  // for (const [eventName, transition] of transitions) {
-  //   const targets = transition[0].target;
-  //   const guard = transition[0].guard;
-
-  //   for (const idx in targets) {
-  //     if (Object.prototype.hasOwnProperty.call(targets, idx)) {
-  //       const srcParentId = parent.id;
-  //       const targetParentId = targets[idx].parent.id;
-  //       if (srcParentId === targetParentId) {
-  //         stringTokens.push(
-  //           "  ".repeat(identation) +
-  //           `${id} --> ${targets[idx].id} :` +
-  //           ` ${transformEventLabel(eventName)}` +
-  //           `${guard ? "/" + guard : ""}\n`);
-  //       } else {
-  //         const siblingStates = targets[idx].parent.states;
-  //         if (siblingStates) {
-  //           const stateName = Object.keys(siblingStates);
-  //           for (const stateIdx in stateName) {
-  //             if (Object.prototype.hasOwnProperty.call(
-  //               stateName, stateIdx)) {
-  //               if (targets[idx].id !== siblingStates[stateName[stateIdx]].id) {
-  //                 stringTokens.push(
-  //                   "  ".repeat(identation) +
-  //                   `${siblingStates[stateName[stateIdx]].id} -->` +
-  //                   ` ${targets[idx].id} :` +
-  //                   ` ${transformEventLabel(eventName)}` +
-  //                   `${guard ? "/" + guard : ""}\n`);
-  //               }
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
   stringTokens = stringTokens.concat(
-    enumerateXStateTransitions(transitions, parent, identation, id));
+    forEachXStateTransitions(
+      transitions, parent, identation, currentId, generateTransition));
 
   if (type === "final") {
-    stringTokens.push(
-      "  ".repeat(identation) +
-      `${id} --> [*]\n`);
+    stringTokens = stringTokens.concat(
+      generateTransitionToFinal(identation, currentId));
   }
 
-  // console.log(stringTokens);
   return stringTokens;
 };
 
@@ -237,6 +309,6 @@ export const generateMermaid = (
   let stringTokens : Array<string> = [];
   stringTokens.push("stateDiagram-v2\n");
   stringTokens = stringTokens.concat(
-    recurseXStateNode(stateMachine.root, 0));
+    visitXStateNode(stateMachine.root, 0));
   return stringTokens.join("");
 };
