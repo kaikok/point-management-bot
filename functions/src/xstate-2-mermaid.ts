9b1d9322f6768ae49538a10a4d39e34d1d31a94f
Kai Kok, Chew <kaikok@gmail.com>
2024-02-19 18:33:14 +0800

    Separated mermaid generation routines to separate files from xstate machine traversal.

--- functions/src/xstate-2-mermaid.ts
+++ functions/src/xstate-2-mermaid.ts
@@ -1,3 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  generateCompositeState,
+  generateConcurrentStates,
+  generateTransition,
+  generateEachConcurrentState,
+  generateInternalState,
+  generateTransitionToFinal,
+  generateStateDeclaration} from "./mermaid-generator";
+
 const eventLabelTransformers = [
   {
     active: true,
@@ -40,89 +50,8 @@ export const getXStateInitialState = (stateMachineNode: any) => {
   return stateMachineNode.states[stateMachineNode.config.initial];
 };
 
-// Mermaid generation routines
-export const generateStateDeclaration = (
-  identation: number,
-  id: string,
-  key: string) => {
-  const stringTokens: Array<string> = [];
-  stringTokens.push(
-    "  ".repeat(identation) +
-    `${id} : ${key}\n`);
-  return stringTokens;
-};
-
-export const generateTransitionToFinal = (
-  identation: number,
-  id: string) => {
-  const stringTokens: Array<string> = [];
-  stringTokens.push(
-    "  ".repeat(identation) +
-    `${id} --> [*]\n`);
-  return stringTokens;
-};
-
-export const generateCompositeState = (
-  currentId: string,
-  initialStateId: string,
-  identation: number,
-  internalStateStringTokens: Array<string>
-): Array<string> => {
-  let stringTokens: Array<string> = [];
-  stringTokens.push("  ".repeat(identation) +
-  `state ${currentId} {\n`);
-  stringTokens.push("  ".repeat(identation + 1) +
-    "[*] --> " +
-    `${initialStateId}\n`);
-  stringTokens = stringTokens.concat(internalStateStringTokens);
-  stringTokens.push("  ".repeat(identation) + "}\n");
-  return stringTokens;
-};
-
-export const generateInternalState = (
-  stateMachineNode: any,
-  identation: number,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  first: boolean,
-  visitNodeFn:
-    (stateMachineNode: any, identation: number) => Array<string>) => {
-  return visitNodeFn(stateMachineNode,
-    identation);
-};
-
-export const generateConcurrentStates = (
-  currentId: string,
-  identation: number,
-  internalStateStringTokens: Array<string>
-): Array<string> => {
-  let stringTokens: Array<string> = [];
-  stringTokens.push("  ".repeat(identation) +
-    `state ${currentId} {\n`);
-  stringTokens = stringTokens.concat(internalStateStringTokens);
-  stringTokens.push("  ".repeat(identation) + "}\n");
-  return stringTokens;
-};
-
-const generateEachConcurrentState = (
-  stateMachineNode: any,
-  identation: number,
-  first: boolean,
-  visitNodeFn:
-    (stateMachineNode: any, identation: number) => Array<string>) => {
-  let stringTokens: Array<string> = [];
-  if (!first) {
-    stringTokens.push(
-      "  ".repeat(identation) +
-      "--\n");
-  }
-  stringTokens = stringTokens.concat(
-    visitNodeFn(stateMachineNode,
-      identation));
-  return stringTokens;
-};
-
 const forEachXStateSubStates = (
-  states: { [x: string]: any; },
+  states: any,
   identation: number,
   callbackFn:
     (stateMachineNode: any, identation: number, first: boolean,
@@ -147,19 +76,6 @@ const forEachXStateSubStates = (
       }
     }
   }
-  return stringTokens;
-};
-
-const generateTransition = (
-  currentStateId: string,
-  targetStateId: string,
-  identation: number,
-  eventName: string, guard: string) => {
-  const stringTokens: Array<string> = [];
-  stringTokens.push("  ".repeat(identation) +
-    `${currentStateId} --> ${targetStateId} :` +
-    ` ${eventName}` +
-    `${guard === "" ? "" : "/" + guard}\n`);
   return stringTokens;
 };
 
@@ -276,7 +192,6 @@ export const visitXStateNode = (
 };
 
 export const generateMermaid = (
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   stateMachine: any) => {
   let stringTokens : Array<string> = [];
   stringTokens.push("stateDiagram-v2\n");
